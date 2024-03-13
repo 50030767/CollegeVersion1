d43e103f5abeef279b1d80becb5df3690a647cb7
50030767 <50030767@serc.ac.uk>
2024-03-13 16:34:48 +0000

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,37 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-      System.out.println("Hello World");
+    public static void main(String[] args)
+    {
+        Student s = new Student("Larry", 52);
+
+        System.out.println(s);
+        System.out.println(s.getName());
+        System.out.println(s.getAge());
+
+        s.setName("Lawrence");
+        s.birthday();
+
+        s.addNickname( "Lazza");
+        s.addNickname(  "Wazza");
+        s.printNicknames();
+
+        System.out.println(s.getName());
+        System.out.println(s.getAge());
+
+        /*Student s2 = new Student("Curly", 54);
+        System.out.println(s2);
+        System.out.println(s2.getName());
+        System.out.println(s2.getAge());
+
+        Module m = new Module("Calculus", 3);
+
+        String aString = m.getName();
+        System.out.println(aString);
+        System.out.println(m.getName()); */
         }
+
+
+
 
 }
